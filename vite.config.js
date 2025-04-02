82bc58b0d532809d695b9816baad7a9277259d21
potchb <potch.ballesteros@orangeandbronze.com>
2025-04-03 03:36:33 +0800

    Add process.env.PORT for deployment

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     // vueDevTools(),
   ],
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
